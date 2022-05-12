73660623f9cd25a19b20d933f2b10b4086384e18
taiki-jp <taiki-dosisha@outlook.jp>
2022-05-12 23:48:36 +0900

    chap3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # http メソッドがGETのアクションは同名のビューを使うことが多い
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root :to => 'コントローラー名#アクション名'
+  root to: 'tasks#index'
 end
