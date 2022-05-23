f4536739e709f88f87d28bd31f07e9c470ae48db
taiki-jp <taiki-dosisha@outlook.jp>
2022-05-24 00:58:52 +0900

    tmp commit

--- db/migrate/20220513121710_create_tasks.rb
+++ db/migrate/20220513121710_create_tasks.rb
@@ -1,5 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :tasks, :name, false
+  # def change
+  #   change_column_null :tasks, :name, false
+  # end
+  def up
+    change_column :tasks, :name, :string, limit: 30
+  end
+
+  def down
+    change_column :tasks, :name, :string
   end
 end
