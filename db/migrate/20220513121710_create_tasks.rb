d4821d3d7022129e504470259468e1e0ae8b948d
taiki-jp <taiki-dosisha@outlook.jp>
2022-05-24 00:37:58 +0900

    feat: update database

--- db/migrate/20220513121710_create_tasks.rb
+++ db/migrate/20220513121710_create_tasks.rb
@@ -1,9 +1,5 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
-    create_table :tasks do |t|
-      t.string :name
-      t.text :description
-      t.timestamps
-    end
+    change_column_null :tasks, :name, false
   end
 end
---
+++ db/migrate/20220523153223_change_t_asks_name_not_null.rb
@@ -0,0 +1,4 @@
+class ChangeTAsksNameNotNull < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
